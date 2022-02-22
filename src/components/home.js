ccc7f3b7c93f31ccc7c079b33a8246c54e2b1513
kylebaugh <kylebaugh1@gmail.com>
2022-02-22 15:10:36 -0700

    added css files

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,17 @@
 import {Link} from 'react-router-dom'
+import ('../cssFiles/home.css')
 
 const Home =  () => {
     
     return (
-        <div>
-            <section class='header'>
+        <div class='homeDiv'>
+            <section class='homeHeader'>
                 <section class='homeLogo'>Inti-Mate</section>
+                {/* <section class='hamburger'>
+                    <section class='hamTop'></section>
+                    <section class='hamMid'></section>
+                    <section class='hamBot'></section>
+                </section> */}
                 <section class='homeLogin'>
                     <Link to='/login'>
                         Login
